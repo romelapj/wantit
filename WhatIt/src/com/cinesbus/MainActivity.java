4bb53a448f32afe87136919ce24e26f616b37da4
Romel Palomino <romelpalomino@hotmail.com>
2013-11-15 10:44:54 -0500

    Interfaz

--- WhatIt/src/com/cinesbus/MainActivity.java
+++ WhatIt/src/com/cinesbus/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity implements View.OnClickListener,
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		//Inicio de Sesion
 		u = new Util(this);
 
 		mPlusClient = new PlusClient.Builder(this, this, this).setActions(
